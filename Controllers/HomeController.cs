439eafb522f31c8f736be2aeef4bd9a1dd2c68fc
talkt <talkt@LOGU_N_J>
2026-01-16 00:00:11 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SSECheck.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        [HttpGet("events")]
+        public async Task Events(CancellationToken ct)
+        {
+            Response.Headers.Add("Content-Type", "text/event-stream");
+            Response.Headers.Add("Cache-Control", "no-cache");
+            Response.Headers.Add("Connection", "keep-alive");
+            Response.Headers.Add("X-Accel-Buffering", "no");
+
+            while (!ct.IsCancellationRequested)
+            {
+                await Response.WriteAsync($"data: ping {DateTime.Now}\n\n", ct);
+                await Response.Body.FlushAsync(ct);
+
+                await Task.Delay(5000, ct);
+            }
+        }
+        [HttpGet("test")]
+        public async Task<string> Test(CancellationToken ct)
+        {
+            return await Task.FromResult("Worked");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+// Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ConfigureEndpointDefaults(lo =>
+    {
+        lo.Protocols = HttpProtocols.Http1;
+    });
+});
+builder.Services.AddCors((options) =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+    });
+});
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+// Swagger middleware (DEV only recommended)
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+    options.RoutePrefix = "swagger"; // default
+});
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
